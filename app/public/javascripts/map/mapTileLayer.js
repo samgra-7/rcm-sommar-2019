3776fba831ea0bc96a47510972d10b32ebb0e643
wilant <wilant-6@student.ltu.se>
2019-07-09 12:47:26 +0200

    small fix-worlmap shows on roadmap now also

--- app/public/javascripts/map/mapTileLayer.js
+++ app/public/javascripts/map/mapTileLayer.js
@@ -71,7 +71,7 @@ const roadTileLayer = L.TileLayer.boundaryCanvas(swedenRoads, {
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets',
-    boundary: countyData
+    //boundary: countyData
 });
 
 /**
